723ba59d03d6e5aa46e340d64a0d2a3977d14669
Carlos Henrique <lc.henriquee@gmail.com>
2024-07-10 02:33:18 -0300

    ..................................................

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build:{
+    rollupOptions:{
+      external: ["magic-panel"]
+    }
+  }
   // build: {
   //   lib: {
   //     entry: 'src/index.tsx',
