a9472ceec3877ad39ee4d8670999b3c9470c0e6f
Carlos Henrique <lc.henriquee@gmail.com>
2024-07-10 03:39:37 -0300

    --------------------

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['magic-panel'],
+    include: ['magic-panel/dist/components/**/*.vue'],
   },
   build: {
     commonjsOptions: {
