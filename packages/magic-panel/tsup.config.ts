96f709e2f23853682f268e0cb709e1d178b1c87e
Carlos Henrique <lc.henriquee@gmail.com>
2024-07-10 17:48:48 -0300

    ---------------------------

--- packages/magic-panel/tsup.config.ts
+++ packages/magic-panel/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, type Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
-  entry: ["./src/index.tsx", "./plugin.ts", "./src/index.css"],
+  entry: ["./src/index.tsx", "./plugin.ts"],
   format: ["esm", "cjs"],
   dts: true,
   external: ["react"],
